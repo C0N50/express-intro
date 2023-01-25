9158c178a29f6cbf84573713c4931192e42e0733
Connor Ferris <ferr0373@gmail.com>
2023-01-25 16:22:29 -0600

    added POST logic

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ const express = require('express');
 //create an express app
 const app = express();
 
+//set up middleware
 app.use(express.static('server/public'));
+
+//set up body parser
+app.use(express.urlencoded({extended:true}));
 
 console.log('What is my purpose? 🤖');
 
@@ -18,10 +22,14 @@ let kitties = [
     'Pala'
 ];
 
+const quotes = [];
+const ratings = [];
+
 
 //"An Endpoint"
 //GET http://localhost:800/utensils
 // this code will run
+
 app.get('/kitties', (req, res) => {
     console.log('Kitties need names!');
     res.send(kitties);
@@ -30,4 +38,20 @@ app.get('/kitties', (req, res) => {
 app.listen(PORT, () => {
     //~onReady()
     console.log('My purpose is to internet! 🤖');
+});
+
+//Post Request
+app.post('/', (req, res) => {
+    console.log('Receiving a POST request!', req.body);
+
+    const quote = req.body.quote;
+    const rating = req.body.rating;
+
+
+    quotes.push([quote, rating]);
+
+    console.log('My quotes and ratings:', quotes);
+
+    res.sendStatus(201); //code says "created"
+
 });
